3fe81ee32b3242b387ce5ba23bbdd265ae2740dc
rochester234 <beccollins82@hotmail.com>
2015-11-29 16:31:24 +0000

    mvp with bootstrap

--- instagram_clone/spec/features/likes_features_spec.rb
+++ instagram_clone/spec/features/likes_features_spec.rb
@@ -7,7 +7,9 @@ feature 'like a picture' do
 
   it 'a user can like a picture, which increments the like count', js: true do
     visit '/pictures'
-    click_link 'Like'
+    within('#pic-link') do
+      click_link "Like"
+    end
     expect(page).to have_content("1 like")
   end
 
